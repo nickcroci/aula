83ee7cedc1bef198bb42c3a3a0422f167deb6eb2
Nicole Croci <nicole.croci@sinqia.com.br>
2024-10-22 14:53:39 -0300

    nhaaa

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log("arquivo para clonagem")
 console.log("nova linha, alterando o projeto localmente")
 //agora um comentário pelo github para testar o comando git pull
+//mais um comentario
