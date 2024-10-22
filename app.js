175a48ad0747ff462c8d4ea35d76d622be92fcb7
nickcroci <68879042+nickcroci@users.noreply.github.com>
2024-10-22 14:54:22 -0300

    nhan

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("arquivo para clonagem")
 console.log("nova linha, alterando o projeto localmente")
 //agora um comentário pelo github para testar o comando git pull
 //mais um comentario
+//mais ummmm 
